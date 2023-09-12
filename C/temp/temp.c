243f0bc38f7415010e2175834dce89d9d9b54e7f
Sabari <education@mindmy.tech>
2023-09-12 09:49:37 +0530

    class stuff

--- C/temp/temp.c
+++ C/temp/temp.c
@@ -3,14 +3,11 @@
 
 int main(void)
 {
-    char string[] = "Yo !";
-    printf("the length of \"%s\" is : %i\n", string, strlen(string));
+    char first_name[]= "Hello";
+    char last_name [] = ", World";
+    printf("the size of first_name is :%i\n", sizeof(first_name));
+    strcat(first_name, last_name);
 
-    int i = 0;
-    while (string[i] != '\0')
-    {
-        i++;
-    }
-    printf("the length of \"%s\" is : %i\n", string, i);
-    
+    printf("Full name : %s\n", first_name);
+    printf("now size of first_name is :%i\n", sizeof(first_name));
 }
