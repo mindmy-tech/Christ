ff560700f90a3c7fcdd971c04a18e9263aeacf8e
Sabari <education@mindmy.tech>
2023-09-16 21:37:47 +0530

    program07 completed!

--- C/Program07/program07.c
+++ C/Program07/program07.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-    char courses[][5] = {"BSc", "BCA","BE"};
-    // reading the total number of bytes alocated and dividing it by 5 
+    char courses[][5] = {"BSC", "BCA", "BE"};
+    char course_details[][500] = {
+        "BSc okay! okay!\n you'll have :\n\tC Programing\n\tMath\n\tAnalog Digital Electronics\n",
+        "BCA Nice Choice\n Don't You worry\n you just have \"Computer Science\"!\n",
+        "BE uhhhh okayyyy\n Math\nPhysics\nChemistry\nand a little bit of computer science!\n" 
+    };
+    // reading the total number of bytes alocated and dividing it by 5
     // coz each course gets 5 bytes
-    int no_courses =  sizeof(courses)/5;
+    int no_courses = sizeof(courses) / 5;
 
     char first_name[12];
-    printf("Name :");
-    // gets(first_name);
-    scanf("%s", &first_name);
+    char last_name[12];
+    do
+    {
+        printf("First Name :");
+        scanf("%s", &first_name);
 
-    printf("%s %i\n", first_name, strlen(first_name));
+        printf("Last Name :");
+        scanf("%s", &last_name);
+    }
 
+    while (strlen(first_name) > 12 || strlen(last_name) > 12);
 
+    char get_course[10];
+    printf("Course :");
+    scanf("%s", &get_course);
+    for (int i = 0; i < strlen(get_course); i++)
+    {
+        get_course[i] = toupper(get_course[i]);
+    }
 
+    for (int i = 0; i < no_courses; i++)
+    {
+        if (!strcmp(get_course, courses[i]))
+        {
+            printf("%s", course_details[i]);
+        }
+    }
 }
